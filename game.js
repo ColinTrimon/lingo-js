acaa257a067b72b2306aa2d124fec43608bf4be4
Colin de Wit <99034809@mydavinci.nl>
2018-01-19 13:56:11 +0100

    laatste bugfixes

--- game.js
+++ game.js
@@ -13,51 +13,64 @@ console.log(res);
 
 function check(){
   getTypedWord();
-console.log(lingo_row);
-      for (var z=0; z<=4; z++) {
-      if (res.includes(document.getElementById("input_"+lingo_row+"_"+z).value)) {
-        document.getElementById("input_"+lingo_row+"_"+z).style.backgroundColor = "yellow";
-      }
-      }
+
       for(var p=0; p<=4; p++){
       if (document.getElementById("input_"+lingo_row+"_"+p).value == res[p]) {
         document.getElementById("input_"+lingo_row+"_"+p).style.backgroundColor = "red";
         delete res[p];
       }
-      console.log(res);
     }
+
+    for (var z=0; z<=4; z++) {
+      if (res.includes(document.getElementById("input_"+lingo_row+"_"+z).value)) {
+        document.getElementById("input_"+lingo_row+"_"+z).style.backgroundColor = "yellow";
+      }
+    }
+
     if (generatedWord===typedWord) {
        alert("gewonnen");
        location.reload();
      }
     wrong++;
-    if (wrong == 5) {
+
+    if (wrong > 5) {
       alert("Verloren");
       location.reload();
     }
-    console.log(res);
     lingo_row++
-    console.log(lingo_row);
-   var main_iets = document.getElementById("game");
-   var br = document.createElement("br")
-   main_iets.appendChild(br)
-   for (var column=0; column <=4; column++){
-   var input = document.createElement("input");
-   input.setAttribute("id", "input_"+lingo_row+"_"+column);
-   input.setAttribute("name","");
-   input.setAttribute("text","");
-   input.setAttribute("value","");
-   input.setAttribute("maxlength","1");
-   input.innerHTM = "";
-   main_iets.appendChild(input);
+
+   setNextrowLingo(lingo_row);
+   setFirstLetter(lingo_row);
+  res = generatedWord.split("");
  }
-document.getElementById("input_"+lingo_row+"_0").value = generatedWord[0];
-res = generatedWord.split("");
+
+
+
+function setFirstLetter(lingo_row)
+{
+  document.getElementById("input_"+lingo_row+"_0").value = generatedWord[0];
+}
+
+function setNextrowLingo(lingo_row)
+{
+  var main_iets = document.getElementById("game");
+  var br = document.createElement("br")
+  main_iets.appendChild(br)
+      for (var column=0; column <=4; column++){
+      var input = document.createElement("input");
+      input.setAttribute("id", "input_"+lingo_row+"_"+column);
+      input.setAttribute("name","");
+      input.setAttribute("text","");
+      input.setAttribute("value","");
+      input.setAttribute("maxlength","1");
+      input.innerHTM = "";
+      main_iets.appendChild(input);
+    }
 }
 
 function getTypedWord(){
   typedWord="";
-  for(var i=0; i < generatedWord.length; i++){
-    typedWord += document.getElementById("input_"+lingo_row+"_"+i).value;
-}
+    for(var i=0; i < generatedWord.length; i++){
+      typedWord += document.getElementById("input_"+lingo_row+"_"+i).value;
+    }
 }
